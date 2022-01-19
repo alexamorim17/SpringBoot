5ec77fa054f07545ac3be7fc0b24a0bfe36b80a9
Alexandre <fulanodetal@exemplo.br>
2022-01-19 12:46:17 -0300

    Adicionando exercícios

--- BlogPessoal2/src/main/java/com/example/demo/Controller/PostagemController.java
+++ BlogPessoal2/src/main/java/com/example/demo/Controller/PostagemController.java
@@ -3,10 +3,16 @@ package com.example.demo.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,6 +45,27 @@ public class PostagemController {
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
 	
+	
+	@PostMapping
+	public ResponseEntity<Postagem> post (@Validated @RequestBody Postagem postagem){
+		return ResponseEntity.status(HttpStatus.CREATED ).body(repository.save(postagem));
+	}
+	
+	@PutMapping
+	public ResponseEntity<Postagem> put (@Validated @RequestBody Postagem postagem){
+		return repository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.ok().body(repository.save(postagem)))
+				.orElse(ResponseEntity.notFound().build());
+	}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> deletePostagem(@PathVariable long id){
+		return repository.findById(id)
+		.map(resposta ->{
+			repository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+		})
+		.orElse(ResponseEntity.notFound().build());
+	}
 	
 	
 
